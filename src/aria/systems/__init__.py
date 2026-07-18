9d3a0c8c3c9dd602afdcbc54c3b7341d66f9d6b2
raincaldrax-hash <raincaldrax@gmail.com>
2026-07-18 04:12:43 +0100

    Update systems __init__ to include all hierarchy systems

--- src/aria/systems/__init__.py
+++ src/aria/systems/__init__.py
@@ -5,5 +5,43 @@ from aria.systems.npc_ai import NPCBehavior
 from aria.systems.items import Item
 from aria.systems.races import Race
 from aria.systems.monsters import Monster
+from aria.systems.celestial_hierarchy import (
+    Archangel,
+    Demigod,
+    CelestialHierarchyManager,
+    CelestialRank
+)
+from aria.systems.demonic_hierarchy import (
+    PrimordialDemon,
+    Demonoid,
+    DemonicHierarchyManager,
+    DemonicRank,
+    DeadlySin
+)
+from aria.systems.dragon_gods import (
+    DragonGod,
+    Wyrmling,
+    DragonGodsManager,
+    DragonType
+)
 
-__all__ = ["AIBase", "NPCBehavior", "Item", "Race", "Monster"]
+__all__ = [
+    "AIBase",
+    "NPCBehavior",
+    "Item",
+    "Race",
+    "Monster",
+    "Archangel",
+    "Demigod",
+    "CelestialHierarchyManager",
+    "CelestialRank",
+    "PrimordialDemon",
+    "Demonoid",
+    "DemonicHierarchyManager",
+    "DemonicRank",
+    "DeadlySin",
+    "DragonGod",
+    "Wyrmling",
+    "DragonGodsManager",
+    "DragonType"
+]
